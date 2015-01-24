fdce0c9ecb40dc7db13e732f1458f7d18fb5f091
Finnegan <Finnegan@Finnegan-PC.hsd1.wa.comcast.net>
2015-01-24 00:12:24 -0800

    light weight java library working with slick 2d, im gonna delete alot of stuff we dont need btw so dont worry if you pull a ton of stuff

--- Project Imperative/src/main/Main.java
+++ Project Imperative/src/main/Main.java
@@ -1,7 +1,26 @@
 package main;
 
-public class Main {
-	public static void main(String[] args){
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Main extends StateBasedGame{
+	
+	public static Main Game = new Main("Game");
+	
+	public Main(String name) {
+		super(name);
+	}
+
+	public static void main(String[] args) throws SlickException{
+		AppGameContainer gameFrame = new AppGameContainer(Game);
+		gameFrame.setTargetFrameRate(60);
+		gameFrame.setDisplayMode(1000, 500, false);		
+		gameFrame.start();		
+	}
+	
+	public void initStatesList(GameContainer gc) throws SlickException {
 		
 	}
 }
