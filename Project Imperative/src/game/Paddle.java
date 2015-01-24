8c030973e3ae018468153424a7a902148b46377a
GhoulPoP <dylan@lamarca.org>
2015-01-24 15:04:09 -0800

    Created the Paddle class

---
+++ Project Imperative/src/game/Paddle.java
@@ -0,0 +1,14 @@
+package game;
+
+import org.newdawn.slick.Color;
+
+public class Paddle extends GameElement {
+	private int POINTS;
+	
+	public Paddle(Board board, int locationX, int locationY) {
+		super(board, 10, 30, locationX, locationY);
+		COLOR = Color.white;
+	}
+
+	
+}
