931c2f93b743dd008ef6458abb81b265dd002173
UltGuardian <alj@hushmail.me>
2015-01-23 20:49:10 -0800

    Added COM_COORDS for interpretMessage in ClientToServerConnection

--- Project Imperative/src/lan/ClientToServerConnection.java
+++ Project Imperative/src/lan/ClientToServerConnection.java
@@ -2,6 +2,8 @@ package lan;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class ClientToServerConnection extends Thread{
 	private DataOutputStream DATA_OUT;
@@ -12,5 +14,24 @@ public class ClientToServerConnection extends Thread{
 		DATA_IN = dataIn;
 	}
 	
+	public void run(){
+		while(true){
+			try {
+				String message = DATA_IN.readUTF();
+				interpretMessage(message);
+			} catch (IOException e) {
+				System.out.println("ClientToServerConnection: Lost connection to the server.");
+			}
+		}
+	}
 	
+	public void interpretMessage(String message){
+		Scanner messageScanner = new Scanner(message);
+		String theCommand = messageScanner.next();
+		if(theCommand.equals(Server.COM_COORDS)){
+			String label = messageScanner.next();
+			int xLocation = messageScanner.nextInt();
+			int yLocation = messageScanner.nextInt();
+		}
+	}
 }
