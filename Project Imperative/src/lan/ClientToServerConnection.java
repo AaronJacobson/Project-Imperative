59f8e38b4cace12e25004c27ff9ef5431903f4d3
UltGuardian <alj@hushmail.me>
2015-01-23 20:44:38 -0800

    Added the connect to server method in Client Started working on ClientToServerConnection

--- Project Imperative/src/lan/ClientToServerConnection.java
+++ Project Imperative/src/lan/ClientToServerConnection.java
@@ -1,5 +1,16 @@
 package lan;
 
-public class ClientToServerConnection {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 
+public class ClientToServerConnection extends Thread{
+	private DataOutputStream DATA_OUT;
+	private DataInputStream DATA_IN;
+	
+	public ClientToServerConnection(DataOutputStream dataOut, DataInputStream dataIn){
+		DATA_OUT = dataOut;
+		DATA_IN = dataIn;
+	}
+	
+	
 }
