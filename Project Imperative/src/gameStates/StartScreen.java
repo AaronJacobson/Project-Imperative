4ddfa1e7f57c78f228ecf9a37f4ac55c14cf11e8
Finnegan <Finnegan@Finnegan-PC>
2015-01-24 12:12:13 -0800

    deleted random test data clutter, and added background syntax

--- Project Imperative/src/gameStates/StartScreen.java
+++ Project Imperative/src/gameStates/StartScreen.java
@@ -3,22 +3,23 @@ package gameStates;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class StartScreen extends BasicGameState{
-
+	private Image background;
 	public void init(GameContainer arg0, StateBasedGame arg1)
 			throws SlickException {
-		
+			background = new Image("slick/testdata/Feild Background.png"); 
 	}
 
 	public void render(GameContainer arg0, StateBasedGame arg1, Graphics g)
 			throws SlickException {
 		g.setColor(Color.blue);
 		g.drawString("herro world!", 300, 300);
-		
+		background.draw();
 	}
 
 	public void update(GameContainer arg0, StateBasedGame arg1, int arg2)
